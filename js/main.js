c98be691771f8543f2e2eb0d6e04c2fca04af4e4
gkm1257 <gkm1257@gmail.com>
2018-02-26 14:23:07 +0800

    feat: Include Knockout library

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 let map;
 let markers = [];
 
+
+let ViewModel = function() {
+	let self = this;  // Make sure we can always access the scope of ViewModel everytime
+
+	
+}
+
 function initMap() {
 	map = new google.maps.Map(document.getElementsByClassName("map")[0], {
 		center: {lat: 24.179018, lng: 120.600357},
