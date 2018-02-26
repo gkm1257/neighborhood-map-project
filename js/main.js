a5f121f1221a54d536fd633e630b16b7706fb075
gkm1257 <gkm1257@gmail.com>
2018-02-26 22:25:50 +0800

    feat: Add slide menu function

--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@ let markers = [];
 let ViewModel = function() {
 	let self = this;  // Make sure we can always access the scope of ViewModel everytime
 
-	
+	this.clickMenu = () => {
+		$(".list-box").toggleClass("menu-hidden");
+		$(".header").toggleClass("expand");
+		$(".map").toggleClass("expand");
+	};
 }
 
 function initMap() {
@@ -13,4 +17,9 @@ function initMap() {
 		center: {lat: 24.179018, lng: 120.600357},
 		zoom: 13
 	});
+	$(".list-box").toggleClass("menu-hidden");
+	$(".header").toggleClass("expand");
+	$(".map").toggleClass("expand");
 }
+
+ko.applyBindings(new ViewModel());
